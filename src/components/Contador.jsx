4d907da0e78ac02d95ed71321c8c7f3c03234e6c
Emilse Arias <emilsearias@gmail.com>
2023-07-12 21:36:55 -0300

    agregue un comentario con la tarea pendiente

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -10,6 +10,7 @@ const [numero, setNumero] = useState(0);
             <h3>Contador</h3>
             <h3>{numero}</h3>
             <Button variant="primary" onClick={()=> setNumero(numero + 1) }>+1</Button>
+            // todo: agregar un boton que reste en 1 el contendido del state
         </section>
     );
 };
